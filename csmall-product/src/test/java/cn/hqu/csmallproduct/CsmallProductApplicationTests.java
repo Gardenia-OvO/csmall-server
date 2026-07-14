2e6b7095d22f8bee2f29ca076d7fa7f2ef59ad2d
LMoring <h_zaychik@outlook.com>
2026-07-14 15:28:34 +0800

    第一次提交

---
+++ csmall-product/src/test/java/cn/hqu/csmallproduct/CsmallProductApplicationTests.java
@@ -0,0 +1,24 @@
+package cn.hqu.csmallproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@SpringBootTest
+class CsmallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Autowired
+    DataSource dataSource;
+    @Test
+    void testDataSource() throws SQLException {
+        System.out.println(dataSource.getConnection());
+    }
+
+}
