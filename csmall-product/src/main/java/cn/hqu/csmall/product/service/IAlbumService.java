3071240e990cb03b80982f65645d306415c29eef
LMoring <h_zaychik@outlook.com>
2026-07-15 09:48:53 +0800

    Service-配置-初编

---
+++ csmall-product/src/main/java/cn/hqu/csmall/product/service/IAlbumService.java
@@ -0,0 +1,4 @@
+package cn.hqu.csmall.product.service;
+
+public interface IAlbumService {
+}
---
+++ csmall-product/src/main/java/cn/hqu/csmall/product/service/impl/AlbumServiceImpl.java
@@ -0,0 +1,8 @@
+package cn.hqu.csmall.product.service.impl;
+
+import cn.hqu.csmall.product.service.IAlbumService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AlbumServiceImpl implements IAlbumService {
+}
