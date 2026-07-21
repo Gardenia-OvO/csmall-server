2c228666b181081a0ad1b6dc2a5e425cba0b4066
LMoring <h_zaychik@outlook.com>
2026-07-21 10:01:20 +0800

    根据ID查询相册详细信息

--- csmall-product/src/main/java/cn/hqu/csmall/product/controller/AlbumController.java
+++ csmall-product/src/main/java/cn/hqu/csmall/product/controller/AlbumController.java
@@ -70,7 +70,7 @@ public class AlbumController {
 
 
     @GetMapping("/standard")
-    @ApiOperation("根据id查询相册详细信息")
+    @ApiOperation("查询相册详细信息")
     @ApiOperationSupport(order = 450)
     @ApiImplicitParam(name = "id", value = "相册id", required = true, dataType = "long")
     public JsonResult standard(@Range(min = 1, message = "根据id删除相册，请提供合法的id")
