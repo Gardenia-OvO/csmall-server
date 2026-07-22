408bc9061f97e4ecbc1c0cc962815a8450c17db8
LMoring <h_zaychik@outlook.com>
2026-07-21 21:50:04 -0400

    新增管理员（mapper批量）

--- csmall-passport/src/main/java/cn/hqu/csmall/passport/controller/AdminController.java
+++ csmall-passport/src/main/java/cn/hqu/csmall/passport/controller/AdminController.java
@@ -1,6 +1,5 @@
 package cn.hqu.csmall.passport.controller;
 
-
 import cn.hqu.csmall.passport.pojo.param.AdminAddNewParam;
 import cn.hqu.csmall.passport.service.IAdminService;
 import cn.hqu.csmall.passport.web.JsonResult;
@@ -10,29 +9,25 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-
+import javax.validation.Valid;
 
 @Slf4j
 @RequestMapping("/admin")
-@Api(tags="01. 管理员管理模块")
+@Api(tags = "01. 管理员管理模块")
 @RestController
 @Validated
 public class AdminController {
     @Autowired
     private IAdminService adminService;
-
     @PostMapping("/add-new")
     @ApiOperation("添加管理员")
-    @ApiOperationSupport
-    public JsonResult addNew(@Validated @RequestBody AdminAddNewParam adminAddNewParam){
-        log.debug("开始处理【新增管理员】的业务，参数：{}",adminAddNewParam);
+    @ApiOperationSupport(order = 100)   //
+    public JsonResult addNew(@Valid AdminAddNewParam adminAddNewParam)  {
+        log.debug("开始处理【添加管理员】的业务，参数：{}",adminAddNewParam);
         adminService.addNew(adminAddNewParam);
-        log.debug("处理【新增管理员】的业务结束");
+        log.debug("处理【添加管理员】的业务结束");
         return JsonResult.ok();
     }
 }
