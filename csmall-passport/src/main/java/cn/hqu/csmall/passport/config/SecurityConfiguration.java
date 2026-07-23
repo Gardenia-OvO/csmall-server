7418f58a089316306911ba212c63f2dc8e39f580
LMoring <h_zaychik@outlook.com>
2026-07-23 10:06:35 +0800

    登陆-明文登陆

--- csmall-passport/src/main/java/cn/hqu/csmall/passport/config/SecurityConfiguration.java
+++ csmall-passport/src/main/java/cn/hqu/csmall/passport/config/SecurityConfiguration.java
@@ -2,13 +2,21 @@ package cn.hqu.csmall.passport.config;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Slf4j
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
--- csmall-passport/src/main/java/cn/hqu/csmall/passport/security/UserDetailsServiceImpl.java
+++ csmall-passport/src/main/java/cn/hqu/csmall/passport/security/UserDetailsServiceImpl.java
@@ -1,7 +1,10 @@
 package cn.hqu.csmall.passport.security;
 
 
+import cn.hqu.csmall.passport.mapper.AdminMapper;
+import cn.hqu.csmall.passport.pojo.vo.AdminLoginInfoVO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,18 +14,23 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private AdminMapper adminMapper;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        log.debug("执行了loadUserByUsername方法，参数是：{}", username);
-        if(!"root".equals(username)){
+        log.debug("Spring Security框架自动调用UserDetailsServiceImpl.loadUserByUsername");
+        //根据用户名查询用户数据
+        AdminLoginInfoVO loginInfoVO = adminMapper.getLoginInfoByUsername(username);
+        if(loginInfoVO == null){
             log.debug("此用户名没有匹配用户数据");
             return  null;
         }
         log.debug("此用户名有匹配用户数据，将返回UserDatils对象");
         UserDetails userDetails = User.builder()
-                .username("root")
-                .password("{noop}123456")
-                .disabled(false)
+                .username(loginInfoVO.getUsername())
+                .password(loginInfoVO.getPassword())
+                .disabled(loginInfoVO.getEnable()==0)
                 .accountLocked(false)
                 .accountExpired(false)
                 .authorities("这是一个临时使用的山寨权限")
